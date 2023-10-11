041caa08c86efea6aadc00098fad60df85cf2bcb
kmtGryffindor20 <tripathikmt20@gmail.com>
2023-10-11 19:01:00 +0530

    something

--- film_fusion/users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.ProfileListCreateAPIView.as_view()),
-    path('register/', views.UserListCreateAPIView.as_view()),
-]
