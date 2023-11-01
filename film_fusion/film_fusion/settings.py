5b617029c2335646299e2b2cf64f429c1e9e8163
kmtGryffindor20 <tripathikmt20@gmail.com>
2023-11-01 16:00:56 +0530

    changed to pyAny

--- film_fusion/film_fusion/settings.py
+++ film_fusion/film_fusion/settings.py
@@ -83,10 +83,10 @@ WSGI_APPLICATION = 'film_fusion.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'filmFusionDB',
-        'USER': 'kaustubh',
-        'PASSWORD': 'testing@123',
-        'HOST':'localhost',
+        'NAME': 'kmtGryffindor20$filmfusionDB',
+        'USER': 'kmtGryffindor20',
+        'PASSWORD': 'testing@321',
+        'HOST':'kmtGryffindor20.mysql.pythonanywhere-services.com',
         'PORT':''
     }
 }
